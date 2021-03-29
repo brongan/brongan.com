c5dd544591551939e131ab06c0bd06bf30844002
Brennan Tracy <brennantracy4@gmail.com>
2021-03-28 22:41:00 -0700

    Switch back to enum from FixedBitSet

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,10 @@ extern crate rand;
 extern crate web_sys;
 mod utils;
 
-use fixedbitset::FixedBitSet;
 use rand::distributions::{Bernoulli, Distribution};
 use rand::thread_rng;
 use std::default::Default;
-// use wasm_bindgen::prelude::*;
+use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -39,18 +38,35 @@ impl<'a> Drop for Timer<'a> {
     }
 }
 
-// #[wasm_bindgen]
+#[wasm_bindgen]
 pub fn set_panic_hook() {
     utils::set_panic_hook();
 }
 
-// #[wasm_bindgen]
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Alive,
+    Dead,
+}
+
+impl Cell {
+    fn toggle(&mut self) {
+        *self = match *self {
+            Cell::Dead => Cell::Alive,
+            Cell::Alive => Cell::Dead,
+        };
+    }
+}
+
+#[wasm_bindgen]
 #[derive(Default)]
 pub struct Universe {
     width: u32,
     height: u32,
-    cells: FixedBitSet,
-    _next: FixedBitSet,
+    cells: Vec<Cell>,
+    _next: Vec<Cell>,
 }
 
 impl Universe {
@@ -102,8 +118,8 @@ impl Universe {
     }
 
     /// Get the dead and alive values of the entire universe.
-    pub fn get_cells(&self) -> &FixedBitSet {
-        &self.cells
+    pub fn get_cells(&self) -> *const Cell {
+        self.cells.as_ptr()
     }
 
     /// Set cells to be alive in a universe by passing the row and column
@@ -112,7 +128,7 @@ impl Universe {
         self.cells.clear();
         for (row, col) in cells.iter().cloned() {
             let idx = self.get_index(row, col);
-            self.cells.set(idx, true);
+            self.cells[idx] = Cell::Alive;
         }
     }
 }
@@ -122,10 +138,9 @@ use std::fmt;
 impl fmt::Display for Universe {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for i in 0..self.width * self.height {
-            let symbol = if !self.cells[i as usize] {
-                '◻'
-            } else {
-                '◼'
+            let symbol = match self.cells[i as usize] {
+                Cell::Alive => '◼',
+                Cell::Dead => '◻',
             };
             write!(f, "{}", symbol)?;
         }
@@ -135,12 +150,12 @@ impl fmt::Display for Universe {
 }
 
 /// Public methods, exported to JavaScript.
-// #[wasm_bindgen]
+#[wasm_bindgen]
 impl Universe {
     pub fn tick(&mut self) {
-        //       let _timer = Timer::new("Universe::tick");
+        let _timer = Timer::new("Universe::tick");
         {
-            // let _timer = Timer::new("new generation");
+            let _timer = Timer::new("new generation");
             for row in 0..self.height {
                 for col in 0..self.width {
                     let idx = self.get_index(row, col);
@@ -150,21 +165,21 @@ impl Universe {
                     let next_cell = match (cell, live_neighbors) {
                         // Rule 1: Any live cell with fewer than two live neighbours
                         // dies, as if caused by underpopulation.
-                        (true, x) if x < 2 => false,
+                        (Cell::Alive, x) if x < 2 => Cell::Dead,
                         // Rule 2: Any live cell with two or three live neighbours
                         // lives on to the next generation.
-                        (true, 2) | (true, 3) => true,
+                        (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
                         // Rule 3: Any live cell with more than three live
                         // neighbours dies, as if by overpopulation.
-                        (true, x) if x > 3 => false,
+                        (Cell::Alive, x) if x > 3 => Cell::Dead,
                         // Rule 4: Any dead cell with exactly three live neighbours
                         // becomes a live cell, as if by reproduction.
-                        (false, 3) => true,
+                        (Cell::Dead, 3) => Cell::Alive,
                         // All other cells remain in the same state.
                         (otherwise, _) => otherwise,
                     };
 
-                    self._next.set(idx, next_cell);
+                    self._next[idx] = next_cell;
                 }
             }
         }
@@ -173,26 +188,29 @@ impl Universe {
     }
 
     pub fn new(width: u32, height: u32) -> Universe {
+        let size = (width * height) as usize;
         Universe {
             width,
             height,
-            cells: Universe::create_cells(width, height),
-            _next: FixedBitSet::with_capacity((width * height) as usize),
+            cells: Universe::create_cells(size),
+            _next: Vec::with_capacity(size),
         }
     }
 
-    fn create_cells(width: u32, height: u32) -> FixedBitSet {
+    fn create_cells(size: usize) -> Vec<Cell> {
         let mut rng = thread_rng();
-        let size = (width * height) as usize;
         let d = Bernoulli::new(0.5).unwrap();
-        let mut cells = FixedBitSet::with_capacity(size);
-        (0..width * height).for_each(|i| cells.set(i as usize, d.sample(&mut rng)));
-        cells
+        (0..size)
+            .map(|_| match d.sample(&mut rng) {
+                true => Cell::Alive,
+                false => Cell::Dead,
+            })
+            .collect()
     }
 
     pub fn reset(&mut self) {
         log!("Resetting Universe");
-        self.cells = Universe::create_cells(self.width, self.height);
+        self.cells = Universe::create_cells((self.width * self.height) as usize);
     }
 
     pub fn kill_all(&mut self) {
@@ -212,46 +230,42 @@ impl Universe {
         self.height
     }
 
-    pub fn cells(&self) -> *const u32 {
-        self.cells.as_slice().as_ptr()
-    }
-
     pub fn toggle_cell(&mut self, row: u32, column: u32) {
         log!("Toggling Cell at: ({}, {})", row, column);
         let idx = self.get_index(row, column);
-        self.cells.toggle(idx);
+        self.cells[idx].toggle();
     }
 
     pub fn insert_pulsar(&mut self, row: u32, column: u32) {
         log!("Inserting Pulsar at: ({}, {})", row, column);
         for (delta_row, delta_col, value) in [
-            (self.height - 4, 1, true),
-            (self.height - 3, 1, true),
-            (self.height - 2, 1, true),
-            (self.height - 1, 1, false),
-            (0, 1, false),
-            (1, 1, false),
-            (2, 1, true),
-            (3, 1, true),
-            (4, 1, true),
-            (self.height - 4, 0, true),
-            (self.height - 3, 0, false),
-            (self.height - 2, 0, true),
-            (self.height - 1, 0, false),
-            (0, 0, false),
-            (1, 0, false),
-            (2, 0, true),
-            (3, 0, false),
-            (4, 0, true),
-            (self.height - 4, self.width - 1, true),
-            (self.height - 3, self.width - 1, true),
-            (self.height - 2, self.width - 1, true),
-            (self.height - 1, self.width - 1, false),
-            (0, self.width - 1, false),
-            (1, self.width - 1, false),
-            (2, self.width - 1, true),
-            (3, self.width - 1, true),
-            (4, self.width - 1, true),
+            (self.height - 4, 1, Cell::Alive),
+            (self.height - 3, 1, Cell::Alive),
+            (self.height - 2, 1, Cell::Alive),
+            (self.height - 1, 1, Cell::Dead),
+            (0, 1, Cell::Dead),
+            (1, 1, Cell::Dead),
+            (2, 1, Cell::Alive),
+            (3, 1, Cell::Alive),
+            (4, 1, Cell::Alive),
+            (self.height - 4, 0, Cell::Alive),
+            (self.height - 3, 0, Cell::Dead),
+            (self.height - 2, 0, Cell::Alive),
+            (self.height - 1, 0, Cell::Dead),
+            (0, 0, Cell::Dead),
+            (1, 0, Cell::Dead),
+            (2, 0, Cell::Alive),
+            (3, 0, Cell::Dead),
+            (4, 0, Cell::Alive),
+            (self.height - 4, self.width - 1, Cell::Alive),
+            (self.height - 3, self.width - 1, Cell::Alive),
+            (self.height - 2, self.width - 1, Cell::Alive),
+            (self.height - 1, self.width - 1, Cell::Dead),
+            (0, self.width - 1, Cell::Dead),
+            (1, self.width - 1, Cell::Dead),
+            (2, self.width - 1, Cell::Alive),
+            (3, self.width - 1, Cell::Alive),
+            (4, self.width - 1, Cell::Alive),
         ]
         .iter()
         .cloned()
@@ -259,22 +273,22 @@ impl Universe {
             let neighbor_row = (row + delta_row) % self.height;
             let neighbor_col = (column + delta_col) % self.width;
             let idx = self.get_index(neighbor_row, neighbor_col);
-            self.cells.set(idx, value);
+            self.cells[idx] = value;
         }
     }
 
     pub fn insert_glider(&mut self, row: u32, column: u32) {
         log!("Inserting Glider at: ({}, {})", row, column);
         for (delta_row, delta_col, value) in [
-            (self.height - 1, 1, true),
-            (0, 1, true),
-            (1, 1, true),
-            (self.height - 1, 0, true),
-            (0, 0, false),
-            (1, 0, false),
-            (self.height - 1, self.width - 1, false),
-            (0, self.width - 1, true),
-            (1, self.width - 1, false),
+            (self.height - 1, 1, Cell::Alive),
+            (0, 1, Cell::Alive),
+            (1, 1, Cell::Alive),
+            (self.height - 1, 0, Cell::Alive),
+            (0, 0, Cell::Dead),
+            (1, 0, Cell::Dead),
+            (self.height - 1, self.width - 1, Cell::Dead),
+            (0, self.width - 1, Cell::Alive),
+            (1, self.width - 1, Cell::Dead),
         ]
         .iter()
         .cloned()
@@ -282,7 +296,7 @@ impl Universe {
             let neighbor_row = (row + delta_row) % self.height;
             let neighbor_col = (column + delta_col) % self.width;
             let idx = self.get_index(neighbor_row, neighbor_col);
-            self.cells.set(idx, value);
+            self.cells[idx] = value;
         }
     }
 }
