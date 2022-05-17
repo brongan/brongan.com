8e889260440290b95acaa983735958bb99a0266e
Brennan Tracy <brennantracy4@gmail.com>
2022-05-17 01:08:09 -0700

    Separate out shaders + GL alias

---
+++ src/basic.vert
@@ -0,0 +1,5 @@
+attribute vec4 aVertexPosition;
+
+void main(void) {
+	gl_Position = aVertexPosition;
+}
--- src/webgl.rs
+++ src/webgl.rs
@@ -3,7 +3,7 @@ use crate::Timer;
 use crate::{UniverseRenderer, Universe};
 
 use wasm_bindgen::JsCast;
-use web_sys::{HtmlCanvasElement, WebGlProgram, WebGlRenderingContext, WebGlShader};
+use web_sys::{HtmlCanvasElement, WebGlProgram, WebGlRenderingContext as GL, WebGlShader};
 
 
 pub struct WebGLRenderer {
@@ -33,23 +33,23 @@ impl UniverseRenderer for WebGLRenderer {
     }
 
     fn render(&mut self, universe: &Universe) {
-        let context = self
+        let gl = self
             .canvas
             .get_context("webgl").unwrap()
             .unwrap()
-            .dyn_into::<WebGlRenderingContext>().unwrap();
+            .dyn_into::<GL>().unwrap();
 
         let _timer = Timer::new("Rendering Frame");
-        self.update_texture(&context, universe);
+        self.update_texture(&gl, universe);
 
         let vertices: [f32; 12] = [
             1.0, 1.0, 0.0, -1.0, 1.0, 0.0, 1.0, -1.0, 0.0, -1.0, -1.0, 0.0,
         ];
 
-        let buffer = context.create_buffer().ok_or("failed to create buffer").unwrap();
-        context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&buffer));
+        let buffer = gl.create_buffer().ok_or("failed to create buffer").unwrap();
+        gl.bind_buffer(GL::ARRAY_BUFFER, Some(&buffer));
 
-        context.use_program(Some(&self.program));
+        gl.use_program(Some(&self.program));
 
         // Note that `Float32Array::view` is somewhat dangerous (hence the
         // `unsafe`!). This is creating a raw view into our module's
@@ -62,21 +62,21 @@ impl UniverseRenderer for WebGLRenderer {
         unsafe {
             let vert_array = js_sys::Float32Array::view(&vertices);
 
-            context.buffer_data_with_array_buffer_view(
-                WebGlRenderingContext::ARRAY_BUFFER,
+            gl.buffer_data_with_array_buffer_view(
+                GL::ARRAY_BUFFER,
                 &vert_array,
-                WebGlRenderingContext::STATIC_DRAW,
+                GL::STATIC_DRAW,
             );
         }
 
-        context.vertex_attrib_pointer_with_i32(0, 3, WebGlRenderingContext::FLOAT, false, 0, 0);
-        context.enable_vertex_attrib_array(0);
+        gl.vertex_attrib_pointer_with_i32(0, 3, GL::FLOAT, false, 0, 0);
+        gl.enable_vertex_attrib_array(0);
 
-        context.clear_color(0.0, 0.0, 0.0, 1.0);
-        context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
+        gl.clear_color(0.0, 0.0, 0.0, 1.0);
+        gl.clear(GL::COLOR_BUFFER_BIT);
 
-        context.draw_arrays(
-            WebGlRenderingContext::TRIANGLE_STRIP,
+        gl.draw_arrays(
+            GL::TRIANGLE_STRIP,
             0,
             (vertices.len() / 3) as i32,
         );
@@ -106,19 +106,19 @@ impl WebGLRenderer {
     const _DEAD_COLOR: &'static str = "#FFFFFF";
     const _ALIVE_COLOR: &'static str = "#000000";
 
-    pub fn update_texture(&mut self, context: &WebGlRenderingContext, universe: &Universe) {
+    pub fn update_texture(&mut self, gl: &GL, universe: &Universe) {
         let level = 0;
-        let internal_format = WebGlRenderingContext::RGBA;
+        let internal_format = GL::RGBA;
         let border = 0;
-        let src_format = WebGlRenderingContext::RGBA;
-        let src_type = WebGlRenderingContext::UNSIGNED_BYTE;
+        let src_format = GL::RGBA;
+        let src_type = GL::UNSIGNED_BYTE;
         let width = self.width;
         let height = self.height;
         let pixel = update_universe_image(&mut self.texture, &universe);
         assert!(pixel.len() == (width * height * 4) as usize);
-        context
+        gl
             .tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_u8_array(
-                WebGlRenderingContext::TEXTURE_2D,
+                GL::TEXTURE_2D,
                 level,
                 internal_format as i32,
                 width as i32,
@@ -131,22 +131,22 @@ impl WebGLRenderer {
             .ok();
 
         if (self.width & (self.width - 1)) == 0 && (self.height & (self.height - 1)) == 0 {
-            context.generate_mipmap(WebGlRenderingContext::TEXTURE_2D);
+            gl.generate_mipmap(GL::TEXTURE_2D);
         } else {
-            context.tex_parameteri(
-                WebGlRenderingContext::TEXTURE_2D,
-                WebGlRenderingContext::TEXTURE_WRAP_S,
-                WebGlRenderingContext::CLAMP_TO_EDGE as i32,
+            gl.tex_parameteri(
+                GL::TEXTURE_2D,
+                GL::TEXTURE_WRAP_S,
+                GL::CLAMP_TO_EDGE as i32,
             );
-            context.tex_parameteri(
-                WebGlRenderingContext::TEXTURE_2D,
-                WebGlRenderingContext::TEXTURE_WRAP_T,
-                WebGlRenderingContext::CLAMP_TO_EDGE as i32,
+            gl.tex_parameteri(
+                GL::TEXTURE_2D,
+                GL::TEXTURE_WRAP_T,
+                GL::CLAMP_TO_EDGE as i32,
             );
-            context.tex_parameteri(
-                WebGlRenderingContext::TEXTURE_2D,
-                WebGlRenderingContext::TEXTURE_MIN_FILTER,
-                WebGlRenderingContext::LINEAR as i32,
+            gl.tex_parameteri(
+                GL::TEXTURE_2D,
+                GL::TEXTURE_MIN_FILTER,
+                GL::LINEAR as i32,
             );
         }
     }
@@ -164,76 +164,51 @@ impl WebGLRenderer {
         let size = (width * height) as usize;
         let texture: Vec<u8> = vec![0; size * 4];
 
-        let context = canvas
+        let gl = canvas
             .get_context("webgl").unwrap()
             .unwrap()
-            .dyn_into::<WebGlRenderingContext>().unwrap();
+            .dyn_into::<GL>().unwrap();
+
+        let vert_code = include_str!("./basic.vert");
+        let frag_code = include_str!("./basic.frag");
 
         let vert_shader = compile_shader(
-            &context,
-            WebGlRenderingContext::VERTEX_SHADER,
-            r#"
-            attribute vec4 aVertexPosition;
-
-            void main(void) {
-              gl_Position = aVertexPosition;
-            }
-    "#,
-        ).unwrap();
+            &gl,
+            GL::VERTEX_SHADER,
+            vert_code).unwrap();
 
         let frag_shader = compile_shader(
-            &context,
-            WebGlRenderingContext::FRAGMENT_SHADER,
-            r#"
-            precision mediump float;
+            &gl,
+            GL::FRAGMENT_SHADER,
+            frag_code).unwrap();
 
-            uniform float vpw; // Width, in pixels
-            uniform float vph; // Height, in pixels
-
-            uniform vec2 offset; // offsets are for nerds
-            uniform vec2 pitch; // idk like the cell size or something
-
-            uniform sampler2D uSampler; // give me cells
-
-            void main() {
-              vec2 newCoord = vec2(vpw * gl_FragCoord.x, vph * gl_FragCoord.y);
-
-              if (int(mod(newCoord.x, pitch[0])) == 0 || int(mod(newCoord.y, pitch[1])) == 0) {
-                gl_FragColor = vec4(0.0, 0.0, 0.0, 0.5);
-              } else {
-                gl_FragColor = texture2D(uSampler, gl_FragCoord.xy / vec2(vpw, vph));
-              }
-            }
-    "#,
-        ).unwrap();
-
-        let program = link_program(&context, &vert_shader, &frag_shader).unwrap();
-        context.use_program(Some(&program));
-        let pitch_loc = context.get_uniform_location(&program, "pitch");
-        context.uniform2fv_with_f32_array(
+        let program = link_program(&gl, &vert_shader, &frag_shader).unwrap();
+        gl.use_program(Some(&program));
+        let pitch_loc = gl.get_uniform_location(&program, "pitch");
+        gl.uniform2fv_with_f32_array(
             pitch_loc.as_ref(),
             &[
                 (WebGLRenderer::CELL_SIZE + 1) as f32,
                 (WebGLRenderer::CELL_SIZE + 1) as f32,
             ],
         );
-        let offset_loc = context.get_uniform_location(&program, "offset");
-        context.uniform2fv_with_f32_array(offset_loc.as_ref(), &[1.0, 1.0]);
-        let vpw_loc = context.get_uniform_location(&program, "vpw");
-        context.uniform1f(vpw_loc.as_ref(), canvas.width() as f32);
-        let vph_loc = context.get_uniform_location(&program, "vph");
-        context.uniform1f(vph_loc.as_ref(), canvas.height() as f32);
-        let usampler_loc = context.get_uniform_location(&program, "uSampler");
+        let offset_loc = gl.get_uniform_location(&program, "offset");
+        gl.uniform2fv_with_f32_array(offset_loc.as_ref(), &[1.0, 1.0]);
+        let vpw_loc = gl.get_uniform_location(&program, "vpw");
+        gl.uniform1f(vpw_loc.as_ref(), canvas.width() as f32);
+        let vph_loc = gl.get_uniform_location(&program, "vph");
+        gl.uniform1f(vph_loc.as_ref(), canvas.height() as f32);
+        let usampler_loc = gl.get_uniform_location(&program, "uSampler");
 
         // Tell WebGL we want to affect texture unit 0
-        context.active_texture(WebGlRenderingContext::TEXTURE0);
+        gl.active_texture(GL::TEXTURE0);
 
         // Tell the shader we bound the texture to texture unit 0
-        context.uniform1i(usampler_loc.as_ref(), 0);
+        gl.uniform1i(usampler_loc.as_ref(), 0);
 
         // Create and bind the texture to texture unit 0
-        let webgl_texture = context.create_texture();
-        context.bind_texture(WebGlRenderingContext::TEXTURE_2D, webgl_texture.as_ref());
+        let webgl_texture = gl.create_texture();
+        gl.bind_texture(GL::TEXTURE_2D, webgl_texture.as_ref());
 
         Some(WebGLRenderer {
             canvas,
@@ -246,51 +221,51 @@ impl WebGLRenderer {
 }
 
 pub fn compile_shader(
-    context: &WebGlRenderingContext,
+    gl: &GL,
     shader_type: u32,
     source: &str,
 ) -> Result<WebGlShader, String> {
     log::info!("Compiling Shader");
-    let shader = context
+    let shader = gl
         .create_shader(shader_type)
         .ok_or_else(|| String::from("Unable to create shader object"))?;
-    context.shader_source(&shader, source);
-    context.compile_shader(&shader);
+    gl.shader_source(&shader, source);
+    gl.compile_shader(&shader);
 
-    if context
-        .get_shader_parameter(&shader, WebGlRenderingContext::COMPILE_STATUS)
+    if gl
+        .get_shader_parameter(&shader, GL::COMPILE_STATUS)
         .as_bool()
         .unwrap_or(false)
     {
         Ok(shader)
     } else {
-        Err(context
+        Err(gl
             .get_shader_info_log(&shader)
             .unwrap_or_else(|| String::from("Unknown error creating shader")))
     }
 }
 
 pub fn link_program(
-    context: &WebGlRenderingContext,
+    gl: &GL,
     vert_shader: &WebGlShader,
     frag_shader: &WebGlShader,
 ) -> Result<WebGlProgram, String> {
-    let program = context
+    let program = gl
         .create_program()
         .ok_or_else(|| String::from("Unable to create shader object"))?;
 
-    context.attach_shader(&program, vert_shader);
-    context.attach_shader(&program, frag_shader);
-    context.link_program(&program);
+    gl.attach_shader(&program, vert_shader);
+    gl.attach_shader(&program, frag_shader);
+    gl.link_program(&program);
 
-    if context
-        .get_program_parameter(&program, WebGlRenderingContext::LINK_STATUS)
+    if gl
+        .get_program_parameter(&program, GL::LINK_STATUS)
         .as_bool()
         .unwrap_or(false)
     {
         Ok(program)
     } else {
-        Err(context
+        Err(gl
             .get_program_info_log(&program)
             .unwrap_or_else(|| String::from("Unknown error creating program object")))
     }
