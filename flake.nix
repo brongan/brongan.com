fb58b388ef41bbbb494f20b5e64998ad855ff907
Brennan Tracy <brennantracy4@gmail.com>
2023-06-15 21:29:14 -0700

    Lsp format.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     crane = {
       url = "github:ipetkov/crane";
-	  inputs = {
+      inputs = {
         nixpkgs.follows = "nixpkgs";
         rust-overlay.follows = "rust-overlay";
         flake-utils.follows = "flake-utils";
@@ -20,43 +20,43 @@
   };
   outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system overlays;
-			config.allowUnfree = true;
+      let
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          config.allowUnfree = true;
+        };
+        overlays = [ (import rust-overlay) ];
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          targets = [ "x86_64-unknown-linux-musl" ];
+        };
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+        commonArgs = {
+          inherit src;
+        };
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        bin = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+          CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+        });
+        dockerImage = pkgs.dockerTools.streamLayeredImage {
+          name = "catscii";
+          tag = "latest";
+          contents = [ bin ];
+          config = {
+            Cmd = [ "${bin}/bin/catscii" ];
+            Env = with pkgs; [ "GEOLITE2_COUNTRY_DB=${clash-geoip}/etc/clash/Country.mmdb" ];
           };
-          overlays = [ (import rust-overlay) ];
-		  rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-			  targets = [ "x86_64-unknown-linux-musl" ];
-		  };
-		  craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-		  src = craneLib.cleanCargoSource (craneLib.path ./.);
-          commonArgs = {
-            inherit src;
+        };
+      in
+      with pkgs;
+      {
+        packages =
+          {
+            inherit bin dockerImage;
+            default = bin;
           };
-		  cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-          bin = craneLib.buildPackage (commonArgs // {
-            inherit cargoArtifacts;
-			CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
-			CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
-          });
-		  dockerImage = pkgs.dockerTools.streamLayeredImage {
-			  name = "catscii";
-			  tag = "latest";
-			  contents = [ bin ];
-			  config = {
-				  Cmd = [ "${bin}/bin/catscii" ];
-				  Env = with pkgs; [ "GEOLITE2_COUNTRY_DB=${clash-geoip}/etc/clash/Country.mmdb" ];
-			  };
-		  };
-        in
-        with pkgs;
-        {
-          packages =
-		    {
-              inherit bin dockerImage;
-              default = bin;
-            };
-		}
-	);
+      }
+    );
 }
