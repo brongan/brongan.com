3ab0c85c323e18b788530e4598133acd0a4dc270
Brennan Tracy <brennantracy4@gmail.com>
2023-07-07 11:15:01 -0700

    Fix bad trap on develop script.

--- develop.sh
+++ develop.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-trap 'kill 0' SIGINT
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 trunk serve --proxy-backend="http://[::1]:8081/api/" &
 cargo watch -- cargo run --bin server -- --port 8081 --static-dir ./dist
