4810ada357af16c8ce23672b4cada5a2c14abd18
Brennan Tracy <brennantracy4@gmail.com>
2023-07-25 23:17:57 -0700

    Switch back to glibc to fix nix build.

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
           targets = [ "wasm32-unknown-unknown" ];
         };
         nativeToolchain = pkgs.rust-bin.stable.latest.default.override {
-          targets = [ "x86_64-unknown-linux-musl" ];
+          targets = [ "x86_64-unknown-linux-gnu" ];
         };
         wasmCraneLib = (crane.mkLib pkgs).overrideToolchain wasmToolchain;
         nativeCraneLib = (crane.mkLib pkgs).overrideToolchain nativeToolchain;
@@ -50,13 +50,14 @@
         };
         commonArgs = {
           inherit src;
-          pname = "brongan.com";
           version = "0.1.0";
         };
+        buildInputs = with pkgs; [ sqlite ];
         nativeArgs = commonArgs // {
           pname = "server";
-          CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
-          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+          CARGO_BUILD_TARGET = "x86_64-unknown-linux-gnu";
+          # CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+          inherit buildInputs;
         };
         cargoArtifacts = nativeCraneLib.buildDepsOnly nativeArgs;
         myServer = nativeCraneLib.buildPackage (nativeArgs // {
@@ -81,7 +82,7 @@
           tag = "latest";
           contents = [ myServer myClient ];
           config = {
-            Cmd = [ "${myServer}/bin/server" "--static-dir=\"\"" ];
+            Cmd = [ "${myServer}/bin/server" ];
             Env = with pkgs; [ "GEOLITE2_COUNTRY_DB=${clash-geoip}/etc/clash/Country.mmdb" ];
           };
         };
