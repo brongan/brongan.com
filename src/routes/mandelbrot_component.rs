55186ceea459d30eed810e91b48d4d594a3e3b0d
Brennan Tracy <brennantracy4@gmail.com>
2024-02-14 22:42:04 -0800

    Client only js stuff is hard.

--- src/routes/mandelbrot_component.rs
+++ src/routes/mandelbrot_component.rs
@@ -1,19 +1,50 @@
 use crate::mandelbrot::Bounds;
+use anyhow::anyhow;
+use image::io::Reader as ImageReader;
 use leptos::*;
+use leptos_dom::html::Canvas;
 use log::info;
 use num::Complex;
-
-#[cfg(not(feature = "ssr"))]
-use wasm_bindgen::{Clamped, JsCast};
+use std::io::Cursor;
+use wasm_bindgen::Clamped;
+use web_sys::CanvasRenderingContext2d;
 
 #[component]
-#[cfg(not(feature = "ssr"))]
-pub fn show_mandelbrot(image: image::RgbaImage) -> impl IntoView {
-    use anyhow::anyhow;
-    use image::RgbaImage;
-    use leptos::html::Canvas;
-    use web_sys::CanvasRenderingContext2d;
+pub fn view_mandelbrot(bounds: Bounds) -> impl IntoView {
+    #[server(GetMandelbrot, "/api", "Cbor", "mandelbrot")]
+    pub async fn get_mandelbrot(
+        bounds: Bounds,
+        upper_left: Complex<f64>,
+        lower_right: Complex<f64>,
+    ) -> Result<Vec<u8>, ServerFnError> {
+        let image =
+            crate::mandelbrot::generate_mandelbrot_multithreaded(bounds, upper_left, lower_right);
+        let mut image_bytes: Vec<u8> = Vec::new();
+        image
+            .write_to(
+                &mut std::io::Cursor::new(&mut image_bytes),
+                image::ImageOutputFormat::Png,
+            )
+            .unwrap();
+        Ok(image_bytes)
+    }
 
+    fn get_mandelbrot_image(image: Vec<u8>) -> Result<image::RgbaImage, ServerFnError> {
+        Ok(ImageReader::new(Cursor::new(image))
+            .with_guessed_format()
+            .unwrap()
+            .decode()
+            .unwrap()
+            .into())
+    }
+
+    info!("loading mandelbrot image");
+    let upper_left = Complex::<f64> {
+        re: -1.20,
+        im: 0.35,
+    };
+    let lower_right = Complex::<f64> { re: -1.0, im: 0.20 };
+    let image = get_mandelbrot_image(get_mandelbrot(bounds, upper_left, lower_right));
     let image = web_sys::ImageData::new_with_u8_clamped_array_and_sh(
         Clamped(image.as_raw()),
         image.width(),
@@ -35,93 +66,49 @@ pub fn show_mandelbrot(image: image::RgbaImage) -> impl IntoView {
     ctx.put_image_data(&image, 0.0, 0.0).unwrap();
 
     view! {
-        <canvas _ref=canvas_element type="text"/>
+        <canvas ref=canvas_element type="text"/>
     }
-}
-
-#[component]
-#[cfg(not(feature = "ssr"))]
-fn render_mandelbrot(
-    bounds: Bounds,
-    upper_left: Complex<f64>,
-    lower_right: Complex<f64>,
-) -> impl IntoView {
-    info!("generating mandelbrot image");
-    let image = crate::mandelbrot::generate_mandelbrot(bounds, upper_left, lower_right);
-    view! { <ShowMandelbrot image/> }
-}
-
-#[component]
-#[cfg(feature = "ssr")]
-fn render_mandelbrot(
-    bounds: Bounds,
-    upper_left: Complex<f64>,
-    lower_right: Complex<f64>,
-) -> impl IntoView {
-    view! { <p> Hello from SSR. </p> }
-}
-
-#[server(GetMandelbrot, "/api", "Cbor", "mandelbrot")]
-pub async fn get_mandelbrot(
-    bounds: Bounds,
-    upper_left: Complex<f64>,
-    lower_right: Complex<f64>,
-) -> Result<Vec<u8>, ServerFnError> {
-    let image =
-        crate::mandelbrot::generate_mandelbrot_multithreaded(bounds, upper_left, lower_right);
-    let mut image_bytes: Vec<u8> = Vec::new();
-    image
-        .write_to(
-            &mut std::io::Cursor::new(&mut image_bytes),
-            image::ImageOutputFormat::Png,
-        )
-        .unwrap();
-    Ok(image_bytes)
-}
-
-#[component]
-fn load_mandelbrot(
-    bounds: Bounds,
-    upper_left: Complex<f64>,
-    lower_right: Complex<f64>,
-) -> impl IntoView {
-    info!("loading mandelbrot image");
-    view! {
-        <header class="header">
-            <h1> { "Mandelbrot" } </h1>
-        </header>
-        <div class="readout">
-            <RenderMandelbrot bounds={bounds} upper_left={upper_left} lower_right={lower_right} />
-        </div>
-        <footer class="footnote">
-            <p><a href="https://github.com/HBBrennan/brongan.com" target="_blank">{ "source" }</a></p>
-        </footer>
-    }
-}
-
-#[component]
-pub fn mandelbrot_model(bounds: Bounds) -> impl IntoView {
-    info!("mandelbrot");
-    let upper_left = Complex::<f64> {
-        re: -1.20,
-        im: 0.35,
-    };
-    let lower_right = Complex::<f64> { re: -1.0, im: 0.20 };
+    let mandelbrot = create_resource(
+        || (),
+        move |_| async move {
+            get_mandelbrot(bounds, upper_left, lower_right)
+                .await
+                .unwrap()
+        },
+    );
+    let (_pending, set_pending) = create_signal(false);
 
     view! {
-        <header class="header">
-            <h1> { "Mandelbrot" } </h1>
-        </header>
-        <div class="readout">
-            <RenderMandelbrot bounds={bounds} upper_left={upper_left} lower_right={lower_right} />
+        <div class="ascii-art">
+            <Transition
+                fallback=move || view! {  <p>"Loading..."</p>}
+                set_pending
+            >
+            {
+                move || match mandelbrot.get() {
+                    Some(image) => {
+                        let image = get_mandelbrot_image(image).unwrap();
+                        view! { <ShowMandelbrot image/> }.into_view()
+                },
+                    None => view! { <p>"Loading..."</p> }.into_view(),
+                }
+            }
+            </Transition>
         </div>
-        <footer class="footnote">
-            <p><a href="https://github.com/HBBrennan/brongan.com" target="_blank">{ "source" }</a></p>
-        </footer>
     }
 }
 
 #[component]
 pub fn mandelbrot() -> impl IntoView {
-    view! { <MandelbrotModel bounds={Bounds {width: 800, height: 500}} /> }
+    view! {
+        <header class="header">
+            <h1> { "Mandelbrot" } </h1>
+        </header>
+        <div class="readout">
+             <LoadMandelbrot bounds={Bounds {width: 800, height: 500}} />
+        </div>
+        <footer class="footnote">
+            <p><a href="https://github.com/HBBrennan/brongan.com" target="_blank">{ "source" }</a></p>
+        </footer>
+    }
 }
