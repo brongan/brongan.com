19ae267aed497fa52a2ff88b1b3cf8b118ad6c50
DJ Petersen <thedjpetersen@gmail.com>
2025-01-26 20:25:00 -0800

    Fix error

--- develop.sh
+++ develop.sh
@@ -8,7 +8,7 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 export RUST_LOG="debug,hyper=info,mio=info"
 
 # Start trunk with more verbose output
-trunk serve --verbose --proxy-backend="https://[::1]:8443/api/" &
+trunk serve --proxy-backend="https://[::1]:8443/api/" &
 
 # Start the backend with additional debugging
 RUST_BACKTRACE=1 cargo watch -x "run --bin server -- --port 8081 --ssl-port 8443 --static-dir client/dist --cert-dir cert"
