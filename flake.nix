54083f2592bb72d9f39b5dfc5d14699ce16976bc
Brennan Tracy <brennantracy4@gmail.com>
2025-12-01 23:31:49 -0800

    Update wasm-bindgen to 0.2.105 because Arch btw.

--- flake.nix
+++ flake.nix
@@ -74,13 +74,13 @@
         wasmBindgenCli = pkgs.buildWasmBindgenCli rec {
           src = pkgs.fetchCrate {
             pname = "wasm-bindgen-cli";
-            version = "0.2.104";
-            hash = "sha256-9kW+a7IreBcZ3dlUdsXjTKnclVW1C1TocYfY8gUgewE=";
+            version = "0.2.105";
+            hash = "sha256-zLPFFgnqAWq5R2KkaTGAYqVQswfBEYm9x3OPjx8DJRY=";
           };
           cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
             inherit src;
             inherit (src) pname version;
-            hash = "sha256-V0AV5jkve37a5B/UvJ9B3kwOW72vWblST8Zxs8oDctE=";
+            hash = "sha256-a2X9bzwnMWNt0fTf30qAiJ4noal/ET1jEtf5fBFj5OU=";
           };
         };
 
